d08925dc8ea0a21ef10b5ecac5e645440c4e27f4
Sandeep Rana <rana.sandeep3119@gmail.com>
2022-03-03 20:22:10 +0530

    setup.sh modified

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\n
+
+echo "\
 [server]\n\
-port=$POST\n\
-enableCORS= false\n\
-headless= true\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
